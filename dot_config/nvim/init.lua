32cceb2c159244290f72890724e98d0e696c84ee
tabsp <tabsp@qq.com>
2022-03-16 01:25:04 +0800

    update treesitter config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -142,6 +142,18 @@ require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true,
   },
+  indent = {
+    enable = true
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
   textobjects = {
     select = {
       enable = true,
